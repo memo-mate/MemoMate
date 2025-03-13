0b09caa8032c57c2c31a0e542fae34ef63b4a588
Daoji <daoji.chang@gmail.com>
2025-03-13 11:30:52 +0800

    ✨ feat: 更新 llm抽象类

--- app/rag/llm/completions.py
+++ app/rag/llm/completions.py
@@ -1,7 +1,7 @@
 from enum import StrEnum
 
-from langchain_core.language_models.base import LanguageModelInput
-from langchain_core.messages import AIMessage
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import Runnable
 from langchain_ollama import ChatOllama
 from langchain_openai import ChatOpenAI
 from pydantic import BaseModel
@@ -14,9 +14,19 @@ class ModelAPIType(StrEnum):
     OLLAMA = "Ollama"
 
 
+RESULT_PROMPT = ChatPromptTemplate.from_template(
+    """回答以下问题，基于提供的上下文信息。如果无法从上下文中找到答案，请说"我不知道"。
+
+上下文: {context}
+问题: {question}
+
+回答:"""
+)
+
+
 class LLMParams(BaseModel):
     prompt: str
-    temperature: float = 0.5
+    temperature: float = 0
     max_tokens: int | None = None
     timeout: int | None = None
     max_retries: int = 3
@@ -28,11 +38,21 @@ class LLMParams(BaseModel):
     base_url: str | None = settings.OPENAI_API_BASE
 
 
+class RAGLLMPrompt(BaseModel):
+    prompt: ChatPromptTemplate = RESULT_PROMPT
+    context: str
+    question: str
+
+
 class LLM:
     def __init__(self) -> None:
         pass
 
-    def generate(self, prompt: LanguageModelInput, params: LLMParams) -> AIMessage:
+    def generate(self, prompt: RAGLLMPrompt, params: LLMParams) -> Runnable:
+        prompt_vars = prompt.prompt.input_variables
+        if "context" not in prompt_vars or "question" not in prompt_vars:
+            raise ValueError("Prompt must have context and question variables.")
+
         match params.api_type:
             case ModelAPIType.OPENAI:
                 llm = ChatOpenAI(
@@ -61,4 +81,6 @@ class LLM:
             case _:
                 raise ValueError("Unsupported api type.")
 
-        return llm.invoke(prompt)
+        chain = prompt.prompt | llm
+        # chain.invoke(prompt, config=config)
+        return chain
