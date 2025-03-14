59adfa8bf46a7f7e8d1851353661c2ef57985c06
Daoji <daoji.chang@gmail.com>
2025-03-14 10:40:31 +0800

    ✨ feat: 流式协议示例

---
+++ app/api/demo/sse.py
@@ -0,0 +1,49 @@
+import asyncio
+from collections.abc import AsyncGenerator
+
+from fastapi import APIRouter, Request
+from pydantic import BaseModel
+from sse_starlette import EventSourceResponse
+
+from app.api.deps import CurrentUser
+from app.core.log_adapter import logger
+from app.models import User
+
+router = APIRouter()
+
+
+class SSEMessage(BaseModel):
+    event: str  # 事件类型
+    id: str  # 消息ID
+    retry: int  # 重连间隔时间 milliseconds
+    data: str  # 消息内容
+
+
+@router.get("/sse", response_model=None, description="SSE消息Demo")
+async def sse(request: Request, current_user: User = CurrentUser):
+    logger.info("SSE消息认证成功", user=current_user.username)
+
+    async def sse_generator() -> AsyncGenerator[dict, None]:
+        try:
+            while True:
+                # 客户端主动断开
+                if await request.is_disconnected():
+                    logger.info(f"Disconnected from client {request.client}")
+                    break
+
+                message = SSEMessage(event="message", id="1", retry=3000, data="Hello, world!")
+                yield message.model_dump_json()
+                await asyncio.sleep(1)
+                message.id = "2"
+                message.data = "Hello, world! 2"
+                yield message.model_dump_json()
+                await asyncio.sleep(1)
+                message.id = "3"
+                message.data = "Hello, world! 3"
+                yield message.model_dump_json()
+        except asyncio.CancelledError:
+            print("sse_generator cancelled")
+        except Exception as e:
+            logger.exception("sse_generator error", exc_info=e)
+
+    return EventSourceResponse(sse_generator(), send_timeout=5)
--- app/api/main.py
+++ app/api/main.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 
+from app.api.demo import sse, websocket
 from app.api.routes import auth, login, user, utils
 
 api_router = APIRouter()
@@ -7,3 +8,5 @@ api_router.include_router(login.router, tags=["login"])
 api_router.include_router(user.router, prefix="/user", tags=["user"])
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
+api_router.include_router(sse.router, prefix="/demo", tags=["demo"])
+api_router.include_router(websocket.router, prefix="/demo", tags=["demo"])
