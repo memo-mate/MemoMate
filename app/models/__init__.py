e3a037f1ece904c9bfec86fea7f59dddbf5f63b0
Daoji <daoji.chang@gmail.com>
2025-04-02 17:33:02 +0800

    ✨ feat: 增加表导出

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,2 +1,4 @@
 # from app.models.action_log import *  # noqa: F403
+from app.models.history_message import *  # noqa: F403
+from app.models.task import *  # noqa: F403
 from app.models.user import *  # noqa: F403
